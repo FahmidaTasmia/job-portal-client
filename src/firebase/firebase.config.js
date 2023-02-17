b15ad73e6111b919d56b3b7270f2a9b64493e240
FahmidaTasmia <tasmia1070@gmail.com>
2023-02-17 20:24:49 +0600

    login register part done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB-sh9ntgzc8UZ6BqZFndZqyKqDZLV3WTw",
+  authDomain: "job-portal-f871b.firebaseapp.com",
+  projectId: "job-portal-f871b",
+  storageBucket: "job-portal-f871b.appspot.com",
+  messagingSenderId: "443934850163",
+  appId: "1:443934850163:web:d62f0ea34b4ae8a388be06"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
